fc97c861a8510f38b5c37771bf254d7a7e508a14
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-07 14:01:05 -0300

    Criando o projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace editor_html_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
