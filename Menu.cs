e9fa0772db253e1f9f6fb419a98568640d98706f
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-07 15:17:25 -0300

    Menu da aplicação

--- Menu.cs
+++ Menu.cs
@@ -12,6 +12,10 @@ namespace editor_html_csharp
       Console.ForegroundColor = ConsoleColor.Black;
 
       DrawScreen();
+      WriteOptions();
+
+      var option = short.Parse(Console.ReadLine());
+      HandleMenuOption(option);
     }
 
     public static void DrawScreen()
@@ -50,5 +54,50 @@ namespace editor_html_csharp
 
     }
 
+    public static void WriteOptions()
+    {
+      Console.SetCursorPosition(3, 2);
+      Console.WriteLine("Editor HTML");
+
+      Console.SetCursorPosition(3, 3);
+      Console.WriteLine("============");
+
+      Console.SetCursorPosition(3, 4);
+      Console.WriteLine("Selecione uma opção abaixo:");
+
+      Console.SetCursorPosition(3, 6);
+      Console.WriteLine("1 - Novo arquivo");
+
+      Console.SetCursorPosition(3, 7);
+      Console.WriteLine("2 - Abrir arquivo");
+
+      Console.SetCursorPosition(3, 9);
+      Console.WriteLine("0 - Sair");
+
+      Console.SetCursorPosition(3, 10);
+      Console.Write("Opção: ");
+    }
+
+    public static void HandleMenuOption(short option)
+    {
+
+      switch (option)
+      {
+        case 1: Console.WriteLine("Editor"); break;
+        case 2: Console.WriteLine("View"); break;
+        case 0:
+          {
+            Console.Clear();
+            Sair();
+            break;
+          }
+        default: Show(); break;
+      }
+    }
+
+    public static void Sair()
+    {
+      Environment.Exit(0);
+    }
   }
 }
