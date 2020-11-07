87bf83695edd6c220f506d9a6cdf4a67579c08d4
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-07 14:57:22 -0300

    Desenhando a tela

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace editor_html_csharp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Menu.Show();
     }
+  }
 }
