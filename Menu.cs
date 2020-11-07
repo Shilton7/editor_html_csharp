9b7c5290ec6d432be04b75f5c6418e800023fbd5
shilton <shilton.santiago@animaeducacao.com.br>
2020-11-07 16:48:36 -0300

    Editor

--- Menu.cs
+++ Menu.cs
@@ -83,7 +83,7 @@ namespace editor_html_csharp
 
       switch (option)
       {
-        case 1: Console.WriteLine("Editor"); break;
+        case 1: Editor.Show(); break;
         case 2: Console.WriteLine("View"); break;
         case 0:
           {
